77e275b11691b08a804da8ac70c055a571c278df
ElsatheFrozen <81809525+ElsatheFrozen@users.noreply.github.com>
2021-11-18 18:38:50 +0530

    Add files via upload

---
+++ toys.js
@@ -0,0 +1,52 @@
+img = "";
+Status = "";
+objects = [];
+
+function preload() {
+    img = loadImage('Toys.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(Status != "")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "There are 6 big objects in the image from which cocossd model has detected " + objects.length + " objects";
+
+            fill('#FF0000');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke('#FF0000');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
